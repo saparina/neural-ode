fc5eafe4c46b2eede5d272c328b71be95a6fbb0f
Michael <konobeev.michael@gmail.com>
2019-03-10 22:25:36 +0300

    Minor fixes and updes

--- experiments/simple_ode.py
+++ experiments/simple_ode.py
@@ -9,70 +9,53 @@ data_size = 50
 batch_time = 10
 batch_size = 30
 
-true_y0 = tf.convert_to_tensor([[0.5, 0.3]], dtype=tf.float32)
-true_A = tf.convert_to_tensor([[-0.1, -1.], [1., -0.1]],  dtype=tf.float32)
+true_y0 = np.array([[0.5, 0.3]], dtype=np.float32)
+true_A = np.array([[-0.1, -1.], [1., -0.1]],  dtype=np.float32)
 t = tf.linspace(0., 25., data_size)
 
-# TODO with keras layer
-# class f(tf.keras.Model):
-#     def call(self, y, t):
-#         return tf.matmul(y, true_A)
 
-def f(y, t):
-    return tf.matmul(y, true_A)
+class Model(tf.keras.Model):
+    def call(self, y, t):
+        return tf.matmul(y, true_A)
 
-true_y= tf.contrib.integrate.odeint(lambda y0, t: f(y0, t), true_y0, t)
-
+f = Model()
+true_y = tf.contrib.integrate.odeint(lambda y0, t: f(y0, t), true_y0, t)
 print(true_y.shape)
 
 
 def get_batch():
     s = np.random.choice(
-        np.arange(data_size - batch_time,
-                  dtype=np.int64), batch_size,
-        replace=False)
-
-    temp_y = true_y.numpy()
-    batch_y0 = tf.convert_to_tensor(temp_y[s])
+        np.arange(data_size - batch_time, dtype=np.int64),
+        batch_size, replace=False)
+    true_y_np = true_y.numpy()
+    batch_y0 = tf.convert_to_tensor(true_y_np[s])
     batch_t = t[:batch_time]
-    batch_y = tf.stack([temp_y[s + i] for i in range(batch_time)], axis=0)
+    batch_y = tf.stack([true_y_np[s + i] for i in range(batch_time)], axis=0)
     return batch_y0, batch_t, batch_y
 
 
-class OdeTrained(tf.keras.Model):
-    def __init__(self, **kwargs):
-        super(OdeTrained, self).__init__(**kwargs)
-
-        self.x = tf.keras.layers.Dense(50, activation='relu',
-                                       kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
-        self.y = tf.keras.layers.Dense(2,
-                                       kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
-
-    def call(self, y, t):
-        y = tf.cast(y, tf.float32)
-        x = self.x(y)
-        y = self.y(x)
-        return y
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(50, input_shape=(2,), activation=tf.nn.relu),
+    tf.keras.layers.Dense(2),
+])
+model_func = lambda *args, **kwargs: model(*args, **kwargs)
 
 
-func = OdeTrained()
 lr = 1e-3
 optimizer = tf.train.AdamOptimizer(lr)
 niters = 10
 log_freq = 2
-for itr in range(1, niters + 1):
+for i in range(1, niters + 1):
+  with tf.GradientTape() as tape:
+    batch_y0, batch_t, batch_y = get_batch()
+    pred_y = odeint(model_func, batch_y0, batch_t)
+    loss = tf.reduce_mean(tf.abs(pred_y - batch_y))
 
-    with tf.GradientTape() as tape:
-        batch_y0, batch_t, batch_y = get_batch()
-        pred_y = odeint(func, batch_y0, batch_t)
-        loss = tf.reduce_mean(tf.abs(pred_y - batch_y))
+  grads = tape.gradient(loss, model.variables)
+    # grad_vars = zip(grads, model.variables)
+    # optimizer.apply_gradients(grad_vars)
 
-    grads = tape.gradient(loss, func.variables)
-    grad_vars = zip(grads, func.variables)
-    optimizer.apply_gradients(grad_vars)
-
-    if itr % log_freq == 0:
-        pred_y = odeint(func, true_y0, t)
-        loss = tf.reduce_mean(tf.abs(pred_y - true_y))
-        print('Iter {} | Loss {}'.format(itr, loss.numpy()))
-
+    # if i % log_freq == 0:
+    #     pred_y = odeint(model_func, true_y0, t, model.variables)
+    #     loss = tf.reduce_mean(tf.abs(pred_y - true_y))
+    #     print('Iter {} | Loss {}'.format(i, loss.numpy()))
--- neuralode/odeint.py
+++ neuralode/odeint.py
@@ -8,46 +8,59 @@ tf.register_tensor_conversion_function(
     lambda value, *args, **kwargs: tf.zeros(0))
 
 
+def _flat(tensor):
+  return tf.reshape(tensor, (-1,))
+
+
 def odeint_grad(grad_output, func, yt, t, args):
-  ysize = yt.shape[1].value
+  yshape = yt.shape[1:]
+  ysize = tf.reduce_prod(yshape)
+
   def backward_dynamics(state, t, args):
-    funcstate = state[:ysize]
+    funcstate = tf.reshape(state[:ysize], yshape)
     adjoint = state[ysize:2 * ysize]
-    fval = func(funcstate, t, args)
-    grad = tfe.gradients_function(
-        lambda *args: tf.tensordot(func(*args), -adjoint, 1))
-    state_grad, t0_grad, adjoint_grad = grad(funcstate, t, args)
+    # TODO: compute vjps with adjoint.
     return tf.concat([fval, state_grad, t0_grad[None], adjoint_grad], 0)
 
   y_grad = grad_output[-1]
   t0_grad = 0
   time_grads = []
-  args_grad = tf.zeros_like(args)
-  for i in range(ysize - 1, 0, -1):
-    time_grads.append(
-        tf.tensordot(func(yt[i], t[i], args), grad_output[i], 1))
-    t0_grad = t0_grad - time_grads[-1]
+  flat_args = tf.concat([_flat(arg) for arg in args], 0)
+  args_grad = tf.zeros_like(flat_args)
+  for i in range(yt.shape[0].value - 1, 0, -1):
+    new_t_grad = tf.tensordot(_flat(func(yt[i], t[i], args)),
+                              _flat(grad_output[i]), 1)
+    time_grads.append(new_t_grad)
+    t0_grad = t0_grad - new_t_grad
 
     rev_t = tf.convert_to_tensor([t[i], t[i - 1]])
-    backward_state = tf.concat([yt[i], y_grad, t0_grad[None], args_grad], 0)
+    backward_state = tf.concat([_flat(yt[i]), _flat(y_grad), t0_grad[None],
+                                args_grad], 0)
     _t = -rev_t
-    _base_reverse_f = backward_dynamics
-    fc = lambda y, t, args: [-f_ for f_ in _base_reverse_f(y,-t, args)]
+    fc = lambda y, t, args: [-val for val in backward_dynamics(y, -t, args)]
 
     backward_answer = tf.contrib.integrate.odeint(
         lambda y0, t: fc(y0, t, args),
         y0=backward_state,
         t=_t)[-1]
     _, y_grad, t_grad, args_grad = tf.split(backward_answer,
-                                            [ysize, ysize, 1, ysize])
+                                            [ysize, ysize, 1, -1])
     y_grad = y_grad + grad_output[i - 1, :]
   time_grads.append(t0_grad)
   time_grads.reverse()
   time_grads = tf.convert_to_tensor(time_grads)
+  args_grad = tf.split(args_grad, [tf.reduce_prod(arg.shape) for arg in args])
+  args_grad = [tf.reshape(g, arg.shape) for g, arg in zip(args_grad, args)]
   return None, y_grad, time_grads, args_grad
 
 
 @tf.custom_gradient
-def odeint(func, y0, t, args):
-  yt = tf.contrib.integrate.odeint(lambda y0, t: func(y0, t, args), y0, t)
-  return yt, lambda grad_output: odeint_grad(grad_output, func, yt, t, args)
+def odeint(func, y0, t):
+  yt = tf.contrib.integrate.odeint(func, y0, t)
+
+  def grad_fn(grad_output, variables=None):
+    _, y_grad, time_grads, var_grad = odeint_grad(
+        grad_output, func, yt, t, variables)
+    return None, y_grad, time_grads, var_grad
+
+  return yt, grad_fn
