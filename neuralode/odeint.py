ca09262313f22b5e85162e02e46e4896639fbc91
Michael <konobeev.michael@gmail.com>
2019-03-15 16:48:07 +0300

    Minor improvements to odeint

--- neuralode/odeint.py
+++ neuralode/odeint.py
@@ -5,8 +5,9 @@ tfe = tf.contrib.eager
 
 
 tf.register_tensor_conversion_function(
-    FunctionType,
-    lambda value, *args, **kwargs: tf.zeros(0))
+    FunctionType, lambda value, *args, **kwargs: tf.zeros(0))
+tf.register_tensor_conversion_function(
+    tf.keras.Model, lambda value, *args, **kwargs: tf.zeros(0))
 
 
 def _flatten(tensors):
@@ -42,7 +43,10 @@ def odeint_grad(grad_output, func, yt, t, variables=None,
     vjp_t = tf.zeros_like(t)
     vjp_y = _flatten(vjp_y)
     vjp_variables = _flatten(vjp_variables)
-    return tf.concat([_flatten(fval), vjp_y, vjp_t[None], vjp_variables], 0)
+
+    # use negative values to integrate from t[i] to t[i - 1] using
+    # tf.contrib.integrate.odeint.
+    return -tf.concat([_flatten(fval), vjp_y, vjp_t[None], vjp_variables], 0)
 
   y_grad = grad_output[-1]
   t_grad = 0
@@ -50,24 +54,18 @@ def odeint_grad(grad_output, func, yt, t, variables=None,
   flat_variables = _flatten(variables)
   variables_grad = tf.zeros_like(flat_variables)
   for i in range(yt.shape[0].value - 1, 0, -1):
-    new_t_grad = tf.tensordot(_flatten(func(yt[i], t[i], variables)),
+    new_t_grad = tf.tensordot(_flatten(func(yt[i], t[i])),
                               _flatten(grad_output[i]), 1)
     time_grads.append(float(new_t_grad))
     t_grad = float(t_grad - new_t_grad)
 
-    rev_t = tf.convert_to_tensor([t[i], t[i - 1]])
     backward_state = tf.concat([_flatten(yt[i]), _flatten(y_grad),
                                 [t_grad], variables_grad], 0)
-
-    _t = -rev_t
-    neg_backward_dynamics = (
-        lambda *args, **kwargs:
-        [-val for val in backward_dynamics(*args, **kwargs)])
-
+    backward_t = tf.convert_to_tensor([-t[i], -t[i - 1]])
     backward_answer = tf.contrib.integrate.odeint(
-        neg_backward_dynamics,
-        y0=backward_state,
-        t=_t, rtol=rtol, atol=atol)[-1]
+        backward_dynamics,
+        y0=backward_state, t=backward_t,
+        rtol=rtol, atol=atol)[-1]
     _, y_grad, t_grad, variables_grad = tf.split(backward_answer,
                                                  [ysize, ysize, 1, -1])
     y_grad = y_grad + _flatten(grad_output[i - 1, :])
